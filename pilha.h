a294d27e59a08719b60d6edd06da3adc77467edd
gleison pires <gleisonaps@hotmail.com>
2022-06-21 22:27:12 -0300

    outro

--- pilha.h
+++ pilha.h
@@ -1,25 +1,28 @@
-#ifndef _PILHA_H_    // para evitar a inclusão múltipla deste arquivo
-#define _PILHA_H_
 #include <stdbool.h>
-#include "data.h"
 
+typedef struct pilha_lista Pilha_List;
 typedef struct pilha Pilha;
 
+struct pilha_lista  {
+
+    bool tipo; // if true, operando, if false, operador
+    float operando;
+    char operador;
+    Pilha_List* prox;
+};
 
 struct pilha {
 
-    Data_t* prim;
+    Pilha_List* prim;
 };
 
 // cria uma pilha vazia
 Pilha* pilha_cria ();
 //empilha dado v no topo da pilha p
-void pilha_push (Pilha* p, char v, float x);
+void pilha_push (Pilha* p, float v);
 //remove e retorna o dado no topo da pilha p;
 float pilha_pop (Pilha* p);
 //retorna true se a pilha p não tiver dados -funcion ok
 bool pilha_vazia (Pilha* p);
 //libera a mameoria ocupada (destroi a pilha p) - funcion ok
 void pilha_libera (Pilha* p);
-
-#endif // _PILHA_H_
