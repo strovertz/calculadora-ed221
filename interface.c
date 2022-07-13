d2a9d6899f04303517d9c49d2575282f0c487d45
gleison pires <gleisonaps@hotmail.com>
2022-07-13 14:41:06 -0300

    Atualizando

--- interface.c
+++ interface.c
@@ -11,7 +11,7 @@ bool teste_tipo(char x){
 }
 
 char* aloca_temp() {
-    char* tmp = malloc(1*sizeof(char));
+    char* tmp = malloc(2*sizeof(char));
     return tmp;
 }
 
@@ -19,20 +19,20 @@ void le_string(Pilha* n, Pilha* c){
     char leitura[30];
     char operador;
     char operando[10];
-    char *temp = malloc(1*sizeof(char));
     int tam;
     char *tmp = aloca_temp();
     strcpy(leitura,"10+3");
     printf("leitura: %s", leitura);
     tam = strlen(leitura);
+
     for(int i = 0; i < tam; i++) {
         while(teste_tipo(leitura[i]) == true){
             tmp[0] = leitura[i];
-            temp[0] = tmp[0];
-            strcat(operando, temp);
+            strcat(operando, tmp);
             calc_operando(n, operando);
             i++;
         }
+
         operador = leitura[i];
         calc_operador(c, n, operador);
         operador = 'z';
